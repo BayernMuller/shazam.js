bc26e95f6b1cca84497cce5ce53dc8a89f1d600d
BayernMuller <cjjunk29@gmail.com>
2024-02-24 20:16:22 +0900

    refactor: simplify memory handling and function call in Vibra class

--- src/index.ts
+++ src/index.ts
@@ -17,24 +17,17 @@ export class Vibra extends EventEmitter {
       throw new Error('Vibra not initialized');
     }
 
-    // 메모리 할당
     const dataPtr = Module._malloc(rawwav.length);
-
-    // Uint8Array 데이터를 Emscripten의 힙 메모리에 복사
     Module.HEAPU8.set(rawwav, dataPtr);
 
-    // Emscripten을 통해 정의된 함수 호출
     const signaturePtr = Module.ccall(
-        'GetSignature',    // C 함수 이름
-        'string',          // 반환 타입
-        ['number', 'number'], // 인자 타입
-        [dataPtr, rawwav.length] // 실제 인자
+        'GetSignature',
+        'string',
+        ['number', 'number'],
+        [dataPtr, rawwav.length]
     ) as string;
 
-    // 메모리 해제
     Module._free(dataPtr);
-
-    // 결과 반환
     return signaturePtr;
   }
 }
